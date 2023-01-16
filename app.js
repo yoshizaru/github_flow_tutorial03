4c1a46a1b527c65a4f1a491450a289a413355d07
yoshizaru <htiancun731@gmail.com>
2023-01-16 10:31:52 +0900

    add function done-todo

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-$("#appTodo").click(function () {
+$("#addTodo").click(function () {
   const inputTodo = $("input").val();
   $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
 
   $("input").val("");
+});
+
+$(document).on("change", "input[type=checkbox]", function () {
+  if ($(this).is(":checked")) {
+    $(this).parent().css("text-decoration", "line-through");
+  } else {
+    $(this).parent().css("text-decoration", "none");
+  }
 });
